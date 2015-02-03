e2f490085ab514f4a4d2524ad2ac2a71770bfa22
Jordan Woerner <jordanw2004@gmail.com>
2015-02-03 13:02:13 +0000

    Javadoc changes to Deck and Hand classes.

--- src/distributedpontoon/shared/Deck.java
+++ src/distributedpontoon/shared/Deck.java
@@ -1,9 +1,46 @@
 package distributedpontoon.shared;
 
+import java.util.Collections;
+import java.util.Stack;
+
 /**
- *
+ * A deck of {@link Card}s, containing all the required cards for a complete 
+ * deck.
+ * 
  * @author 6266215
+ * @version 1.0
+ * @since 2015-02-03
  */
 public class Deck {
     
+    Stack<Card> cards;
+    
+    public Deck()
+    {
+        createDeck();
+    }
+    
+    public final void createDeck()
+    {
+        clearDeck();
+        for (Card.CardSuit suit : Card.CardSuit.values())
+            for (Card.CardValue value : Card.CardValue.values())
+                cards.add(new Card(suit, value));
+        shuffle();
+    }
+    
+    public final void shuffle()
+    {
+        Collections.shuffle(cards);
+    }
+    
+    public final Card pullCard()
+    {
+        return cards.pop();
+    }
+    
+    public final void clearDeck()
+    {
+        this.cards = new Stack<>();
+    }
 }
--- src/distributedpontoon/shared/Hand.java
+++ src/distributedpontoon/shared/Hand.java
@@ -1,8 +1,12 @@
 package distributedpontoon.shared;
 
 /**
- *
+ * Represents a hand in a game of Blackjack. Contains zero or more {@link Card} 
+ * objects to be shared between the {@link Client} and {@link Server}.
+ * 
  * @author 6266215
+ * @version 1.0
+ * @since 2015-02-03
  */
 public class Hand {
     
